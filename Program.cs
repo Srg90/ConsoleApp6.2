275706059d8c1a6143b57037a003339f8c6dd953
Srg90 <usikov90@mail.ru>
2021-11-21 20:24:38 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите предложение не используя знаки препинания");
+            Console.WriteLine();
+
+            string S1 = Console.ReadLine();
+            string S2 = S1.Replace(" ", "").ToLower();
+            char [] S3 = S2.ToCharArray();
+            Array.Reverse(S3);
+            string S4 = new string(S3);
+            Console.WriteLine();
+            //Console.WriteLine(S2);
+            //Console.WriteLine();
+            Console.WriteLine(S4);
+            Console.WriteLine();
+
+            bool result = char.Equals(S2, S4);
+            
+            {
+                if (result == true)
+                {
+                    Console.WriteLine("Палиндром найден");
+                   
+                }
+                if (result == false)
+                {
+                    Console.WriteLine("Палиндром не найден");
+                }
+            }
+
+
+
+           
+           
+            Console.ReadKey();
+        }
+    }
+}
